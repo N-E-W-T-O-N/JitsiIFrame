df1010c28709e061a256cd49ac19a17f148fdc32
newton.mallick <newton.mallick@celebaltech.com>
2024-08-27 18:35:17 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using JitsiIFrame.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JitsiIFrame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Jitsi()
+        {
+            return View();
+        }
+
+        public IActionResult JitsiWeb()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/jitsi.js
@@ -0,0 +1,28 @@
+﻿const domain = 'meet.jit.si';
+const options = {
+    roomName: params.get('id'),
+    width: '100%',
+    height: '100%',
+    parentNode: document.querySelector('#meet'),
+    userInfo: {
+        //email: user.email,
+        displayName: user.display_name
+    },
+    configOverwrite: {
+        disableDeepLinking: true,
+    },
+    interfaceConfigOverwrite: {
+        TOOLBAR_BUTTONS: [
+            'microphone', 'camera', 'closedcaptions', 'desktop', 'fullscreen',
+            'fodeviceselection', 'hangup',
+            'etherpad',
+            'videoquality', 'filmstrip', 'feedback',
+            'tileview', 'download', 'help', 'chat'
+        ],
+        SHOW_CHROME_EXTENSION_BANNER: false
+    }
+};
+const api = new JitsiMeetExternalAPI(domain, options);
+api.addEventListener('readyToClose', () => {
+    api.dispose();
+});
